619acc6fbc3274339fa2307b0d01c3f238fdcb27
Matthew Melvin <matthewmelvin@gmail.com>
2024-02-16 11:59:39 +1000

    Add downloadurl

--- config/startup.sh
+++ config/startup.sh
@@ -2,4 +2,14 @@
 
 cd "$(dirname "$0")"
 
-./srcds_run -game dod +sv_lan 1 +map dod_donner
+ip=$(curl -s ident.me | tee /dev/stderr); echo
+ip=10.1.2.58
+port=8082 # docker to map to port 80
+
+map=$(grep ^dod_ dod/cfg/mapcycle.txt | head -1 | tee /dev/stderr)
+
+sudo /etc/init.d/lighttpd start
+
+echo "sv_downloadurl \"http://$ip:$port/\"" > dod/cfg/downloadurl.txt
+
+./srcds_run -game dod +sv_lan 1 +map $map +exec downloadurl.txt
