3d469bcac072fba5afb282bb887ef1562aeb0d25
Matthew Melvin <matthewmelvin@gmail.com>
2024-02-17 23:37:41 +1000

    Common config for lan and internet

--- config/startup.sh
+++ config/startup.sh
@@ -2,9 +2,25 @@
 
 cd "$(dirname "$0")"
 
-ip=$(curl -s ident.me | tee /dev/stderr); echo
-ip=10.1.2.58
-port=8082 # docker to map to port 80
+port=8082
+
+if [ -n "$LANFLAG" ]; then
+	lan="$LANFLAG"
+else
+	lan=1
+fi
+
+if [ -n "$EXTIP" ]; then
+	extip="$EXTIP"
+else
+	extip="$(curl -s ident.me | tee /dev/stderr)"; echo
+fi
+
+if [ -n "$USERTOK" ]; then
+	tok="$USERTOK"
+else
+	tok=""
+fi
 
 map=$(grep ^dod_ dod/cfg/mapcycle.txt | head -1 | tee /dev/stderr)
 
@@ -12,4 +28,8 @@ sudo /etc/init.d/lighttpd start
 
 echo "sv_downloadurl \"http://$ip:$port/\"" > dod/cfg/downloadurl.txt
 
-./srcds_run -game dod +sv_lan 1 +map $map +exec downloadurl.txt
+./srcds_run -game dod \
+	-port 27015 +ip 0.0.0.0 \
+	-strictportbind +sv_lan $lan \
+	${tok:++sv_setsteamaccount $tok} \
+	+map $map +exec downloadurl.txt
